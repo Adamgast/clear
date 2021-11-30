142a3509d51b8bd302f09674b9d447950055fe7c
Adamgast <92505201+Adamgast@users.noreply.github.com>
2021-11-30 20:32:37 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,4 +11,25 @@ function ibg(){
 }
 ibg();
 //=====================================================================================================
+//Динамический адаптив=====================================================================================================
+const parent_orig = document.querySelector('.footer__container');
+const parent = document.querySelector('.company__container');
+const item = document.querySelector('.footer__footer');
+
+	// Слушаем изменение размера экрана
+window.addEventListener('resize', function(event){
+	const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	if (viewport_width<=767) {
+		if (!item.classList.contains('_done')) {
+			parent.insertBefore(item,parent.children[2]);
+			item.classList.add('_done');
+		}
+	} else{
+		if (item.classList.contains('_done')) {
+			parent_orig.insertBefore(item,parent_orig.children[0]);
+			item.classList.remove('_done');
+		}
+	}
+});
+//=====================================================================================================
 
